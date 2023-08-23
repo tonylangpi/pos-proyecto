75efeb76dc24ea7aab1a9cb8a750c210213fde26
LangPiñeiro <alangp@miumg.edu.gt>
2023-08-22 23:36:21 -0600

    agregue perfil logueo

--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -1,14 +1,18 @@
 "use client"
 import { Card} from 'flowbite-react';
+import { useSession, signOut } from "next-auth/react";
 const Perfil = () => {
+  const { data: session, status } = useSession();
+
+  console.log(session);
   return (
     <Card className='basis-2/4 w-40'>
     <div className="flex flex-col">
       <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-        Bonnie Green
+        {session?.user?.nombre}
       </h5>
       <span className="text-sm text-gray-500 dark:text-gray-400">
-        Visual Designer
+      {session?.user?.correo}
       </span>
     </div>
   </Card>
